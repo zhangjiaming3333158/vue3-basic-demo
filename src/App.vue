1c97381931d2f242dc18ce6e187b9052b8869983
zhangjiaming3333158 <1093561079@qq.com>
2023-05-30 23:48:53 +0800

    5_30

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,49 @@
 <script setup>
 import Edit from './components/Edit.vue'
+import { ref,onMounted } from 'vue'
+import axios from 'axios'
 
 // TODO: 列表渲染
-
+const list = ref([])
+const getList = async () => {
+  const res = await axios.get('/list')
+  list.value = res.data
+}
+onMounted(() => getList())
 
 // TODO: 删除功能
-
+const deleteItem = async (row) => {
+  console.log(row.id);
+  await axios.delete(`/del/${row.id}`)
+  getList()
+}
 
 // TODO: 编辑功能
+const edit = ref(null)
+const open = (item) => {
+  edit.value.open(item)
+}
+const updateList = () => {
+  getList()
+}
 
 </script>
 
 <template>
   <div class="app">
-    <el-table :data="[{
-      id: 1,
-      name: 'jack',
-      place: 'none'
-    }]">
+    <el-table :data="list">
       <el-table-column label="ID" prop="id"></el-table-column>
       <el-table-column label="姓名" prop="name" width="150"></el-table-column>
       <el-table-column label="籍贯" prop="place"></el-table-column>
       <el-table-column label="操作" width="150">
-        <template #default>
-          <el-button type="primary" link>编辑</el-button>
-          <el-button type="danger" link>删除</el-button>
+        <template #default="{row}">
+          <el-button type="primary" link @click="open(row)">编辑</el-button>
+          <el-button type="danger" link @click="deleteItem(row)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
   </div>
-  <Edit />
+  <Edit ref="edit" @update-list="updateList"/>
 </template>
 
 <style scoped>
--- src/components/Edit.vue
+++ src/components/Edit.vue
@@ -1,26 +1,54 @@
 <script setup>
 // TODO: 编辑
 import { ref } from 'vue'
+import axios from 'axios'
 // 弹框开关
 const dialogVisible = ref(false)
 
+// form
+const form = ref({
+  name: '',
+  place: '',
+  id: ''
+})
 
+//open dialog
+const open = (item) => {
+  dialogVisible.value = true
+  form.value.name = item.name
+  form.value.place = item.place
+  form.value.id = item.id
+}
+
+//emit
+const emit = defineEmits(['update-list'])
+const emitForm = async () => {
+  await axios.patch(`/edit/${form.value.id}`, form.value)
+  dialogVisible.value = false
+  emit('update-list')
+}
+
+
+//宏定义
+defineExpose({
+  open
+})
 </script>
 
 <template>
   <el-dialog v-model="dialogVisible" title="编辑" width="400px">
     <el-form label-width="50px">
       <el-form-item label="姓名">
-        <el-input placeholder="请输入姓名" />
+        <el-input placeholder="请输入姓名" v-model="form.name" />
       </el-form-item>
       <el-form-item label="籍贯">
-        <el-input placeholder="请输入籍贯" />
+        <el-input placeholder="请输入籍贯" v-model="form.place"/>
       </el-form-item>
     </el-form>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="dialogVisible = false">取消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确认</el-button>
+        <el-button type="primary" @click="emitForm">确认</el-button>
       </span>
     </template>
   </el-dialog>
